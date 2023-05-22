7918bd7abe2b495561ae1e57e116288ab9720119
hebatallah ibrahim <heba_walid1012@gmail.com>
2023-05-22 07:33:01 +0300

    first commit

---
+++ database2/seeders/RolesTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            [
+                'id'    => 1,
+                'title' => 'Admin',
+            ],
+            [
+                'id'    => 2,
+                'title' => 'User',
+            ],
+        ];
+
+        Role::insert($roles);
+
+    }
+}
